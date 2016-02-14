73216e666265362d4218f0859a1355a37473469f
gdvandongen <gdvandongen@gmail.com>
2016-02-14 22:34:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### Simple HTTP GET/PUT/POST Connector for flows
+
+This app let you run HTTP requests from flows. Responses are ignored.
+You can select GET, POST and PUT action cards. Passing of a JSON string is obligatory for GET en POST cards. Happy hacking!
---
+++ app.js
@@ -0,0 +1,60 @@
+"use strict";
+
+var request = require('request');
+
+var self = module.exports = {
+	init: function () {
+		// HTTP Get Request flow action
+		Homey.manager('flow').on('action.http_get', function( callback, args ){
+			Homey.log("Http Get action. Passed parameter: ", args);
+			var url = args.url;
+			request.get(
+				url,
+				{},
+				function (error, response, body) {
+					if (!error && response.statusCode == 200) {
+						// ready
+						callback( null, true);
+					}
+				}
+			)
+		});
+
+		// HTTP Post JSON  Request flow action
+		Homey.manager('flow').on('action.http_post_json', function( callback, args ){
+			Homey.log("Http Post action. Passed parameters: ", args);
+			var url = args.url;
+			var data = JSON.parse(args.data);
+			request({
+				url: url,
+				method: "POST",
+				json: true,
+				body: data
+				}, function (error, response, body) {
+					if (!error && response.statusCode == 200) {
+						// ready
+						callback( null, true);
+					}
+				}
+			)
+		});
+
+		// HTTP Put Request flow action
+		Homey.manager('flow').on('action.http_put_json', function( callback, args ){
+			Homey.log("Http Put action. Passed parameters: ", args);
+			var url = args.url;
+			var data = JSON.parse(args.data);
+			request({
+				url: url,
+				method: "PUT",
+				json: data},
+				function (error, response, body) {
+					if (!error && response.statusCode == 200) {
+						// ready
+						callback( null, true);
+					}
+				}
+			)
+		});
+	}
+}
