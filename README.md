9bac74309cb95cab5809488f9cf1220aeb21e63a
gdvandongen <gdvandongen@gmail.com>
2016-02-15 22:39:50 +0100

    Bugfixes and new cards
    Fixed try/catch syntax error
    Added condition card for GET request
    Added action card for DELETE request

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-### Simple HTTP GET/PUT/POST Connector for flows
+# Simple HTTP GET/PUT/POST Connector for flows
 
 This app let you run HTTP requests from flows. Responses are ignored.
-You can select GET, POST and PUT action cards. Passing of a JSON string is obligatory for GET en POST cards. Happy hacking!
+
+### Action cards:
+- HTTP Delete
+- HTTP Get
+- HTTP Put JSON
+- HTTP Post JSON
+
+Passing of a valid JSON string (like at least {} ) is obligatory for GET en POST cards.  Happy hacking!
+
+### Condition card:
+- HTTP Get Response Code: Checks the HTTP response code of a GET Request.
--- app.js
+++ app.js
@@ -4,34 +4,70 @@ var request = require('request');
 
 var self = module.exports = {
 	init: function () {
-		// HTTP Get Request flow action
-		Homey.manager('flow').on('action.http_get', function( callback, args ){
-			Homey.log("Http Get action. Passed parameter: ", args);
+		// HTTP Get Request flow condition
+		Homey.manager('flow').on('condition.http_get', function( callback, args ){
+			Homey.log("HTTP Get condition. Passed parameters: ", args);
 			var url = args.url;
+			var status_code = args.status_code;
 			request.get(
 				url,
 				{},
+				function (error, response, body) {
+					if (!error) {
+						// ready
+						var result = (response.statusCode == status_code);
+						callback(null, result );
+					} else {
+						callback( error );
+					}
+				}
+			);
+		});
+
+		// HTTP Delete Request flow action
+		Homey.manager('flow').on('action.http_get', function( callback, args ){
+			Homey.log("HTTP Delete action. Passed parameter: ", args);
+			var url = args.url;
+			request.del(
+				url,
 				function (error, response, body) {
 					if (!error && response.statusCode == 200) {
 						// ready
 						callback( null, true);
 					} else {
-						callback( error )
+						callback( error );
 					}
 				}
-			)
+			);
+		});
+
+		// HTTP Get Request flow action
+		Homey.manager('flow').on('action.http_get', function( callback, args ){
+			Homey.log("HTTP Get action. Passed parameter: ", args);
+			var url = args.url;
+			request.get(
+				url,
+				function (error, response, body) {
+					if (!error && response.statusCode == 200) {
+						// ready
+						callback( null, true);
+					} else {
+						callback( error );
+					}
+				}
+			);
 		});
 
 		// HTTP Post JSON  Request flow action
 		Homey.manager('flow').on('action.http_post_json', function( callback, args ){
-			Homey.log("Http Post action. Passed parameters: ", args);
+			Homey.log("HTTP Post action. Passed parameters: ", args);
 			var url = args.url;
 			try {
 			   var data = JSON.parse(args.data);
-			catch(error){
+			} catch(error) {
 			   return callback(error);
 			}
-			
+
 			request({
 				url: url,
 				method: "POST",
@@ -41,19 +77,19 @@ var self = module.exports = {
 						// ready
 						callback( null, true);
 					} else {
-						callback( error )
+						callback( error );
 					}
 				}
-			)
+			);
 		});
 
 		// HTTP Put Request flow action
 		Homey.manager('flow').on('action.http_put_json', function( callback, args ){
-			Homey.log("Http Put action. Passed parameters: ", args);
+			Homey.log("HTTP Put action. Passed parameters: ", args);
 			var url = args.url;
 			try {
 			   var data = JSON.parse(args.data);
-			catch(error){
+			} catch(error){
 			   return callback(error);
 			}
 			request({
@@ -65,10 +101,10 @@ var self = module.exports = {
 						// ready
 						callback( null, true);
 					} else {
-						callback( error )
+						callback( error );
 					}
 				}
-			)
+			);
 		});
 	}
 }
