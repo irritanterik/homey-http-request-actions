03a21bf7b42484bf8bc59dbe7498ff9e38bce86b
gdvandongen <gdvandongen@gmail.com>
2016-03-01 00:03:35 +0100

    Get variable and trigger a flow with it

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ condition or execute a HTTP request as a flow action with this app.
 - Incoming GET requests
  - ```http://<YourHomeyIP>/api/app/com.internet/<event>```
  - ```http://<YourHomeyIP>/api/app/com.internet/<event>/<value>``` will put a value in the 'value'-token of this card.
+- GET variable step 2 (read 'get variable and trigger flow')
 
 ### Condition cards
 - HTTP Get Response: Checks the HTTP response code of a GET Request.
@@ -20,6 +21,22 @@ condition or execute a HTTP request as a flow action with this app.
 - HTTP Post Form (content-type 'application/x-www-form-urlencoded')
 - HTTP Post JSON (content-type 'application/json')
 - WebSocket Send (message to ws://x.x.x.x:y endpoint)
+- GET variable step 1 (read 'get variable and trigger flow')
 
-Passing a valid JSON string (at least {} ) is obligatory for cards with a JSON parameter.
- Happy hacking!
+## Get variable and trigger a flow with it
+The paired action and trigger cards GET variable step 1/2 enables you to get a value from any JSON-formatted get request and start a flow with the retrieved value as a token.
+
+The action card has four parameters:
+ 1. Url
+ 2. JSON encoded query parameters: ?a=1&b=zz use {"a": 1, b: "zz"})
+ 3. [JSONpath formatted](http://jsonpath.com/) expression of desired value. The result of this expression must be a single value.
+ 4. Name of trigger/event for step 2
+
+When this cards executes succesfull, it will start flows with the 'GET variable step 2' trigger card and same trigger/event as step 1. The result of the JSONpath expression is available as a token on the card.
+
+#### Note   
+  Passing a valid JSON string (at least {} ) is obligatory for cards with a JSON parameter.
+    Happy hacking!
+
+#### Todo
+ - Refactor to https://www.npmjs.com/package/http.min
--- app.js
+++ app.js
@@ -1,51 +1,78 @@
-"use strict";
+'use strict'
 
-var request = require("request");
-var last_event = "";
+var jsonPath = require('jsonpath-plus')
+//var http = require('http.min')
+var request = require('request')
+var webSocket = require('ws')
+var last_event = ''
+
+function isArray(a) {
+    return (!!a) && (a.constructor === Array);
+};
+
+function isObject(a) {
+    return (!!a) && (a.constructor === Object);
+};
 
 function flow_triggers() {
-	// Get Request flow trigger with value
-	Homey.manager("flow").on("trigger.http_get", function( callback, args, state ){
-		Homey.log("in flow trigger.http_get with args, state and last_event:", args, state, last_event);
+	// Get request flow trigger with value
+	Homey.manager('flow').on('trigger.http_get', function( callback, args, state ){
+		Homey.log('in flow trigger.http_get with args, state and last_event:', args, state, last_event)
 
 		// change last_event to state.event when flow object is passed correctly
 		if(args.event.toLowerCase() ==  last_event.toLowerCase()){
-			callback( null, true);
+			Homey.log('  --> trigger flow!')
+			callback( null, true)
 		} else {
-		  callback( null, false);
+			Homey.log('  --> do not trigger flow!')
+		  callback( null, false)
 		}
-	});
+	})
+
+	// Get request flow trigger with value
+	Homey.manager('flow').on('trigger.http_get_variable_2', function( callback, args, state ){
+		Homey.log('in flow trigger.http_get_variable_2 with args and state:', args, state)
+
+		// change last_event to state.event when flow object is passed correctly
+		if(args.trigger.toLowerCase() ==  state.trigger.toLowerCase()){
+			Homey.log('  --> trigger flow!')
+			callback( null, true)
+		} else {
+			Homey.log('  --> do not trigger flow!')
+		  callback( null, false)
+		}
+	})
 } // funtion flow_triggers() end
 
 function flow_conditions() {
-	// Get Request Response flow condition
-	Homey.manager("flow").on("condition.http_get", function( callback, args ){
-		Homey.log("HTTP Get condition. Passed parameters: ", args);
-		var url = args.url;
-		var status_code = args.status_code;
+	// Get request Response flow condition
+	Homey.manager('flow').on('condition.http_get', function( callback, args ){
+		Homey.log('HTTP Get condition. Passed parameters: ', args)
+		var url = args.url
+		var status_code = args.status_code
 		request.get({url: url},
 			function (error, response, body) {
 				if (!error) {
 					// ready
-					var result = (response.statusCode == status_code);
-					callback(null, result );
+					var result = (response.statusCode == status_code)
+					callback(null, result )
 				} else {
-					callback( error );
+					callback( error )
 				}
 			}
-		);
-	});
+		)
+	})
 
-	// Get Request Response (JSON) flow condition
-	Homey.manager("flow").on("condition.http_get_json", function( callback, args ){
-		Homey.log("HTTP Get with JSON condition. Passed parameters: ", args);
-		var url = args.url;
+	// Get request Response (JSON) flow condition
+	Homey.manager('flow').on('condition.http_get_json', function( callback, args ){
+		Homey.log('HTTP Get with JSON condition. Passed parameters: ', args)
+		var url = args.url
 		try {
-			 var data = JSON.parse(args.data);
+			 var data = JSON.parse(args.data)
 		} catch(error) {
-			 return callback(error);
+			 return callback(error)
 		}
-		var status_code = args.status_code;
+		var status_code = args.status_code
 		request.get({
 			url: url,
 			qs: data,
@@ -53,58 +80,58 @@ function flow_conditions() {
 			function (error, response, body) {
 				if (!error) {
 					// ready
-					var result = (response.statusCode == status_code);
-					callback(null, result );
+					var result = (response.statusCode == status_code)
+					callback(null, result )
 				} else {
-					Homey.log("HTTP Get with JSON condition. Error in response:", error)
-					callback( error );
+					Homey.log('HTTP Get with JSON condition. Error in response:', error)
+					callback( error )
 				}
 			}
-		);
-	});
+		)
+	})
 } // function flow_conditions() end
 
 function flow_actions() {
-	// HTTP Delete Request flow action
-	Homey.manager("flow").on("action.http_delete", function( callback, args ){
-		Homey.log("HTTP Delete action. Passed parameter: ", args);
-		var url = args.url;
+	// HTTP Delete request flow action
+	Homey.manager('flow').on('action.http_delete', function( callback, args ){
+		Homey.log('HTTP Delete action. Passed parameter: ', args)
+		var url = args.url
 		request.del({url: url},
 			function (error, response, body) {
 				if (!error && response.statusCode == 200) {
 					// ready
-					callback( null, true);
+					callback( null, true)
 				} else {
-					callback( error );
+					callback( error )
 				}
 			}
-		);
-	});
+		)
+	})
 
-	// HTTP Get Request flow action
-	Homey.manager("flow").on("action.http_get", function( callback, args ){
-		Homey.log("HTTP Get action. Passed parameter: ", args);
-		var url = args.url;
+	// HTTP Get request flow action
+	Homey.manager('flow').on('action.http_get', function( callback, args ){
+		Homey.log('HTTP Get action. Passed parameter: ', args)
+		var url = args.url
 		request.get({url: url},
 			function (error, response, body) {
 				if (!error && response.statusCode == 200) {
 					// ready
-					callback( null, true);
+					callback( null, true)
 				} else {
-					callback( error );
+					callback( error )
 				}
 			}
-		);
-	});
+		)
+	})
 
-	// HTTP Get JSON Request flow action
-	Homey.manager("flow").on("action.http_get_json", function( callback, args ){
-		Homey.log("HTTP Get JSON action. Passed parameter: ", args);
+	// HTTP Get JSON request flow action
+	Homey.manager('flow').on('action.http_get_json', function( callback, args ){
+		Homey.log('HTTP Get JSON action. Passed parameter: ', args)
 		var url = args.url;
 		try {
-			 var data = JSON.parse(args.data);
+			 var data = JSON.parse(args.data)
 		} catch(error) {
-			 return callback(error);
+			 return callback(error)
 		}
 		request.get({
 			url: url,
@@ -112,127 +139,176 @@ function flow_actions() {
 			}, function (error, response, body) {
 				if (!error && response.statusCode == 200) {
 					// ready
-					callback( null, true);
+					callback( null, true)
 				} else {
-					callback( error );
+					callback( error )
 				}
 			}
-		);
-	});
+		)
+	})
+
+	// HTTP Get JSON request flow action
+	Homey.manager('flow').on('action.http_get_variable_1', function( callback, args ){
+		Homey.log('GET variable step 1 action. Passed parameters: ', args)
+		var url = args.url
+		try {
+			 var data = JSON.parse(args.data)
+		} catch(error) {
+			 return callback(error)
+		}
+		var path = args.path
+		var trigger = args.trigger
+
+		request.get({
+			url: url,
+			qs: data,
+			json: true
+			}, function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+					//https://jsonpath.curiousconcept.com/
+					var variable = jsonPath({json: body, path: path, wrap: false});
+					console.log('Variable is ', variable)
+
+					if (variable == null ) {
+						console.log('variable == null')
+						callback('Result from jsonPath is null')
+						return
+					}
+					if (isObject(variable)) {
+						console.log('variable is an Object')
+						callback('Result from jsonPath is an Object')
+						return
+					}
+					if (isArray(variable)) {
+						console.log('variable is an Array')
+						callback('Result from jsonPath is an Array')
+						return
+					}
+
+					Homey.manager('flow').trigger('http_get_variable_2',
+						{value: variable},
+						{trigger: trigger}
+					)
+					callback( null, true)
+
+				} else {
+					callback( error )
+				}
+			}
+		)
+	})
 
 	// HTTP Post form flow action
-	Homey.manager("flow").on("action.http_post_form", function( callback, args ){
-		Homey.log("HTTP Post form action. Passed parameters: ", args);
-		var url = args.url;
+	Homey.manager('flow').on('action.http_post_form', function( callback, args ){
+		Homey.log('HTTP Post form action. Passed parameters: ', args)
+		var url = args.url
 		try {
-			 var data = JSON.parse(args.data);
+			 var data = JSON.parse(args.data)
 		} catch(error) {
-			 return callback(error);
+			 return callback(error)
 		}
-		Homey.log("  JSON parsed data: ", data);
+		Homey.log('  JSON parsed data: ', data)
 
 		request({
 			url: url,
-			method: "POST",
+			method: 'POST',
 			form: data
 			}, function (error, response, body) {
 				if (!error && response.statusCode == 200) {
 					// ready
-					callback( null, true);
+					callback( null, true)
 				} else {
-					Homey.log("  HTTP Post form action. Error in response:", error, response)
-					callback( error );
+					Homey.log('  HTTP Post form action. Error in response:', error, response)
+					callback( error )
 				}
 			}
-		);
-	});
+		)
+	})
 
-	// HTTP Post JSON  Request flow action
-	Homey.manager("flow").on("action.http_post_json", function( callback, args ){
-		Homey.log("HTTP Post action. Passed parameters: ", args);
-		var url = args.url;
+	// HTTP Post JSON  request flow action
+	Homey.manager('flow').on('action.http_post_json', function( callback, args ){
+		Homey.log('HTTP Post action. Passed parameters: ', args)
+		var url = args.url
 		try {
-			 var data = JSON.parse(args.data);
+			 var data = JSON.parse(args.data)
 		} catch(error) {
-			 return callback(error);
+			 return callback(error)
 		}
-		Homey.log("  JSON parsed data: ", data);
+		Homey.log('  JSON parsed data: ', data)
 
 		request({
 			url: url,
-			method: "POST",
+			method: 'POST',
 			json: data
 			}, function (error, response, body) {
 				if (!error && response.statusCode == 200) {
 					// ready
-					callback( null, true);
+					callback( null, true)
 				} else {
-					Homey.log("  HTTP Post action. Error in response:", error, response)
-					callback( error );
+					Homey.log('  HTTP Post action. Error in response:', error, response)
+					callback( error )
 				}
 			}
-		);
-	});
+		)
+	})
 
-	// HTTP Put Request flow action
-	Homey.manager("flow").on("action.http_put_json", function( callback, args ){
-		Homey.log("HTTP Put action. Passed parameters: ", args);
-		var url = args.url;
+	// HTTP Put request flow action
+	Homey.manager('flow').on('action.http_put_json', function( callback, args ){
+		Homey.log('HTTP Put action. Passed parameters: ', args)
+		var url = args.url
 		try {
-			 var data = JSON.parse(args.data);
+			 var data = JSON.parse(args.data)
 		} catch(error){
-			 return callback(error);
+			 return callback(error)
 		}
 		request({
 			url: url,
-			method: "PUT",
+			method: 'PUT',
 			json: data},
 			function (error, response, body) {
 				if (!error && response.statusCode == 200) {
 					// ready
-					callback( null, true);
+					callback( null, true)
 				} else {
-					callback( error );
+					callback( error )
 				}
 			}
-		);
-	});
+		)
+	})
 
 	// HTTP Socket flow action
-	Homey.manager("flow").on("action.web_socket_send", function( callback, args ){
-		Homey.log("WebSocket Send action. Passed parameters: ", args);
-		var url = args.url;
-	  var data = args.data;
-
-		var WebSocket = require('ws');
-		var ws = new WebSocket(url);
+	Homey.manager('flow').on('action.web_socket_send', function( callback, args ){
+		Homey.log('webSocket Send action. Passed parameters: ', args)
+		var url = args.url
+	  var data = args.data
+		var ws = new webSocket(url)
 
 		ws.on('open', function() {
 		  ws.send(data, function(){
-					ws.close();
-					Homey.log("  WebSocket Send action completed.");
-					callback( null, true);
-			});
-		});
+					ws.close()
+					Homey.log('  webSocket Send action completed.')
+					callback( null, true)
+			})
+		})
 
 		ws.on('error', function(error) {
-			Homey.log("  WebSocket Send action failed:", error);
-			callback(error);
-		});
-	});
+			Homey.log('  webSocket Send action failed:', error)
+			callback(error)
+		})
+	})
 } // function flow_actions() end
 
 var self = module.exports = {
 	init: function () {
-		flow_triggers();
-		flow_conditions();
-		flow_actions();
+		flow_triggers()
+		flow_conditions()
+		flow_actions()
 	},
 	trigger: function(event, value) {
-		last_event = event; // ugly global
-		Homey.manager("flow").trigger("http_get"
-			,{"value": value}
-			,{"event": event}
-		);
+		last_event = event // ugly global
+		Homey.manager('flow').trigger('http_get'
+			,{'value': value}
+			,{'event': event}
+		)
 	}
 }
