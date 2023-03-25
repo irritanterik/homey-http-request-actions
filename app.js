72bcf1d073bf5f8eb2db936969bec674366b1c8d
snipterik <evandongen@sniptech.nl>
2023-03-25 01:34:52 +0100

    sdk3 condition cards
    migrated condition cards to sdk3

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const Homey = require('homey')
 const util = require('./lib/util.js')
 // const flowActions = require('./lib/flow/actions.js')
-// const flowConditions = require('./lib/flow/conditions.js')
+const flowConditions = require('./lib/flow/conditions.js')
 const flowTriggers = require('./lib/flow/triggers.js')
 
 class HttpApp extends Homey.App {
   async onInit () {
     flowTriggers.init(this.homey)
-    // flowConditions.init()
-    // flowActions.init()
+    flowConditions.init(this.homey)
+    // flowActions.init(this.homey)
   }
 }
 
